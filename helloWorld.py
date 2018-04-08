d492f0856f15b57a585443b2f5c727b9bb90b792
toroden79 <toroden@gmail.com>
2018-04-08 11:40:56 -0500

    Chapter 3 events examples

--- helloWorld.py
+++ helloWorld.py
@@ -1,27 +1,36 @@
 #!/usr/bin/env python
 
-background_image_filename = 'sushiplate.jpg'
+#background_image_filename = 'sushiplate.jpg'
+from statistics import mode
+
+background_image_filename = 'DSCN00472.JPG'
 mouse_image_filename = 'fugu.png'
 
 import pygame
 from pygame.locals import *
 from sys import exit
+#from import curses
 
 pygame.init()
 
-screen = pygame.display.set_mode((640, 480), 0, 32)
+screen = pygame.display.set_mode((640, 480), RESIZABLE, 32)
 pygame.display.set_caption("Hello, World!")
-
+#stdscr=curses.initscr()
 background = pygame.image.load(background_image_filename).convert()
 mouse_cursor = pygame.image.load(mouse_image_filename).convert_alpha()
-
+x2=0
+y2=0
 while True:
+    z=1
 
     for event in pygame.event.get():
         if event.type == QUIT:
             pygame.quit()
             exit()
-
+        z+=1
+#        stdscr.print(0,0,z)
+#        if screen.mode() != pygame.display.set_mode((640, 480), RESIZABLE, 32):
+        screen = pygame.display.set_mode((640, 480), RESIZABLE, 32)
         screen.blit(background, (0,0))
 
         x, y = pygame.mouse.get_pos()
---
+++ showEvents.py
@@ -0,0 +1,19 @@
+import pygame
+from pygame.locals import *
+from sys import exit
+
+pygame.init()
+SCREEN_SIZE = (800, 600)
+screen = pygame.display.set_mode(SCREEN_SIZE,RESIZABLE, 32)
+
+while True:
+
+    for event in pygame.event.get():
+
+        if event.type == QUIT:
+            pygame.quit()
+            exit()
+
+        print(event)
+
+    pygame.display.update()
