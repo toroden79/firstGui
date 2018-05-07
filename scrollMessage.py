26bbacbc45388616b5b46c37dfd016ea71540158
toroden79 <toroden@gmail.com>
2018-05-06 21:59:45 -0500

    Chapter 3 events examples Scrolling Message   Worked on Diagonal scrolling from lower right to upper left.  Fixed up the refreshing so that when entering "pace" mode, the x and y coordinates are only update when the paceInterval has been reached or exceeded to stop the jumping and which actually made it look like the text was scrolling in the opposite direction.   Added constants for window width and height and text message width and height and changed the initial positions and checks for resetting of x and y coordinates.

--- scrollMessage.py
+++ scrollMessage.py
@@ -3,8 +3,11 @@ from pygame.locals import *
 from sys import exit
 
 background_image_filename = 'sushiplate.jpg'
-SCREEN_SIZE = (640, 480)
-message="    This is a demonstration of the scrolly message script. "
+screen_width = 640
+screen_height = 480
+SCREEN_SIZE = (screen_width, screen_height)
+#message="    This is a demonstration of the scrolly message script. "
+message="This is a demonstration of the scrolly message script."
 
 pygame.init()
 screen = pygame.display.set_mode(SCREEN_SIZE)
@@ -12,8 +15,15 @@ screen = pygame.display.set_mode(SCREEN_SIZE)
 font = pygame.font.SysFont("arial", 80);
 text_surface = font.render(message, True, (0, 0, 255))
 
-x = 0
-y = ( SCREEN_SIZE[1] - text_surface.get_height() ) / 2
+#x = 0
+#y = ( SCREEN_SIZE[1] - text_surface.get_height() ) / 2
+
+#total_x = text_surface.get_width() + screen_width
+#total_y = text_surface.get_height() + screen_height
+message_width = text_surface.get_width()
+message_height = text_surface.get_height()
+x=message_width
+y=message_height
 
 background = pygame.image.load(background_image_filename).convert()
 
@@ -32,26 +42,36 @@ while True:
         if (event.type == KEYDOWN):
             if event.key == K_p:
                 paceMe=True
-                paceInterval=200
+                paceInterval=100
             elif event.key == K_s:
                 paceMe=True
-                paceInterval=2000
+                paceInterval=200
             else:
                 paceMe=False
 
     screen.blit(background, (0,0))
 
-    x-= 2
-    if x < -text_surface.get_width():
-        x = 0
+
 
     if paceMe:
-        if pacer>=paceInterval:
+        if pacer>=paceInterval or x < -message_width or y < -message_height:
+            x -= 5
+            y -= 2
+            if x < -message_width or y < -message_height:
+                # x = 0
+                x = screen_width
+                y = screen_height
             screen.blit(text_surface, (x, y))
-            screen.blit(text_surface, (x+text_surface.get_width(), y))
+            #screen.blit(text_surface, (x+text_surface.get_width(), y))
             pygame.display.update()
             pacer=0
     else:
+        x -= 5
+        y -= 2
+        if x < -message_width or y < -message_height:
+            # x = 0
+            x = screen_width
+            y = screen_height
         screen.blit(text_surface, (x, y))
-        screen.blit(text_surface, (x+text_surface.get_width(), y))
+        #screen.blit(text_surface, (x+text_surface.get_width(), y))
         pygame.display.update()
